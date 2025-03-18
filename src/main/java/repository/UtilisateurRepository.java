7048b14724ae7e4fa2459682a9fe17d10535829e
Reda LAKHLEDJ <LAKHLEDJ_Re@lprs.local>
2025-03-18 17:27:37 +0100

    fin du tp1 utilisatuerRepository

--- src/main/java/repository/UtilisateurRepository.java
+++ src/main/java/repository/UtilisateurRepository.java
@@ -9,16 +9,16 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class UtilisateurRepository {
-    private Connection connexion;
+    private Connection cnx;
 
     public UtilisateurRepository() {
-        this.connexion = Database.getConnexion();
+        this.cnx = Database.getConnexion();
     }
 
     public void inscription(Utilisateur utilisateur) {
         String sql = "INSERT INTO utilisateur (nom, prenom, email, mdp) VALUES (?, ?, ?, ?, ?)";
         try {
-            PreparedStatement stmt = connexion.prepareStatement(sql);
+            PreparedStatement stmt = cnx.prepareStatement(sql);
             stmt.setString(1, utilisateur.getNom());
             stmt.setString(2, utilisateur.getPrenom());
             stmt.setString(3, utilisateur.getEmail());
@@ -31,20 +31,21 @@ public class UtilisateurRepository {
         }
     }
 
+
     public void recupererUserParEmail(String email) {
         String sql = "SELECT * FROM utilisateur WHERE email = ?";
         try {
-            PreparedStatement stmt = connexion.prepareStatement(sql);
+            PreparedStatement stmt = cnx.prepareStatement(sql);
             stmt.setString(1, email);
-            ResultSet infoRecup = stmt.executeQuery();
-            if (infoRecup.next()) {
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
                 Utilisateur user = new Utilisateur(
-                        infoRecup.getInt("id_utilisateur"),
-                        infoRecup.getString("nom"),
-                        infoRecup.getString("prenom"),
-                        infoRecup.getString("email"),
-                        infoRecup.getString("mot_de_passe"),
-                        infoRecup.getString("role")
+                       rs.getInt("id_utilisateur"),
+                       rs.getString("nom"),
+                       rs.getString("prenom"),
+                       rs.getString("email"),
+                       rs.getString("mot_de_passe"),
+                       rs.getString("role")
                 );
             }
 
@@ -53,40 +54,93 @@ public class UtilisateurRepository {
         }
     }
 
-    public void connexion(Utilisateur user) {
+    public Utilisateur connexion(Utilisateur user) {
         String sql = "SELECT * FROM utilisateur WHERE email = ? AND mot_de_passe = ?";
         try {
-            PreparedStatement stmt = connexion.prepareStatement(sql);
+            PreparedStatement stmt = cnx.prepareStatement(sql);
             stmt.setString(1, user.getEmail());
             stmt.setString(2, user.getMdp());
-            ResultSet infoRecup = stmt.executeQuery();
-            if (infoRecup.next()) {
-                Utilisateur userCo = new Utilisateur(
-                        infoRecup.getInt("id_utilisateur"),
-                        infoRecup.getString("nom"),
-                        infoRecup.getString("prenom"),
-                        infoRecup.getString("email"),
-                        infoRecup.getString("mot_de_passe"),
-                        infoRecup.getString("role")
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                Utilisateur userr= new Utilisateur(
+                        rs.getInt("id_utilisateur"),
+                        rs.getString("nom"),
+                        rs.getString("prenom"),
+                        rs.getString("email"),
+                        rs.getString("mot_de_passe"),
+                        rs.getString("role")
                 );
-                return userCo;
+
             }
 
         } catch (SQLException e) {
             System.out.println("Erreur lors de l'ajout de l'utilisateur : " + e.getMessage());
         }
+        return user;
     }
 
-    public ArrayList<Utilisateur> getTousLesUtilisateurs() {
+
+
+    public ArrayList<Utilisateur> getTousLesUtilisateurs()throws SQLException {
         String sql = "SELECT * FROM utilisateur";
-        try {
-            PreparedStatement stmt = Utilisateur.prepareStatement(sql);
-            ResultSet infoRecup = stmt.executeQuery();
-            ArrayList<Utilisateur> inscrit = new ArrayList<>();
-            return inscrit;
+            PreparedStatement stmt = cnx.prepareStatement(sql);
+            ArrayList<Utilisateur> utilisateur = new ArrayList<>();
+        ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Utilisateur user = new Utilisateur(
+
+                rs.getInt("id_utilisateur"),
+                rs.getString("nom"),
+                rs.getString("prenom"),
+                rs.getString("email"),
+                rs.getString("mot_de_passe"),
+                rs.getString("role"));
+                 utilisateur.add(user);
+
+
+
         }
 
+            return utilisateur;
+    }
+
+
+    public void supprimerUtilisateurParEmail(String email) throws SQLException {
+        String sql = "DELETE FROM utilisateur WHERE email = ?";
+        try {
+            PreparedStatement stmt = cnx.prepareStatement(sql);
+            stmt.setString(1, email);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("erreur");
+        }
 
     }
 
-}
+    public void mettreAJourUtilisateur(Utilisateur utilisateur) throws SQLException {
+        String sql = "UPDATE utilisateurs SET nom = ?, prenom = ?, mdp = ?, role = ? WHERE email = ?";
+        try{
+            PreparedStatement stmt = cnx.prepareStatement(sql);
+            stmt.setString(1, utilisateur.getNom());
+            stmt.setString(2, utilisateur.getPrenom());
+            stmt.setString(3, utilisateur.getEmail());
+            stmt.setString(4, utilisateur.getMdp());
+            stmt.setString(5, utilisateur.getRole());
+            stmt.setString(6, utilisateur.getEmail());
+            stmt.executeUpdate();
+        }catch (SQLException e){
+            System.out.printf("Erreur : %s\n",e.getMessage());
+        }
+
+    }
+
+
+
+
+
+    }
+
+
+
+
